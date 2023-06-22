d4e9d6acb800a2b9a956810d3e57d8562ed9c1c0
Daniel Fischer <git@danielfischer.name>
2023-06-22 21:47:36 +0300

    Initial commit

---
+++ scripts/finalReport.R
@@ -0,0 +1,23 @@
+if(!is.element("snakemake",ls())){
+  projFolder <- "/scratch/project_2001746/TestProject"
+  pipelineFolder <- "/users/fischerd/git/Snakebite-LabQC/"
+  pipelineConfig.file <- "/scratch/project_2001746/TestProject/Snakebite-LabQC_config.yaml"
+}
+
+last_n_char <- function(x, n=1){
+  substr(x, nchar(x) - n + 1, nchar(x))
+}
+
+if(last_n_char(pipelineFolder)!="/") pipelineFolder <- paste0(pipelineFolder, "/")
+
+createRMD.command <- paste0("cat ",pipelineFolder,"scripts/Rmodules/final-header.Rmd ",
+                                   pipelineFolder,"scripts/Rmodules/helpFunctions.Rmd ",
+                                   pipelineFolder,"scripts/Rmodules/generalWorkflow.Rmd ",
+                                   pipelineFolder,"scripts/Rmodules/basicStats.Rmd ",
+                                   pipelineFolder,"scripts/Rmodules/Alignments.Rmd ",
+                                   "> ",projFolder,"/finalReport.Rmd")
+cat(createRMD.command, "\n")
+
+system(createRMD.command)
+
+rmarkdown::render(file.path(projFolder,"finalReport.Rmd"), output_file=file.path(projFolder,"finalReport.html"))
